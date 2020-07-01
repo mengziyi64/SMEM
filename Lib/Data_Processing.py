e13161bd02851a3bb951a0c21fe172c52c6de017
Ziyi <47070398+mengziyi64@users.noreply.github.com>
2020-07-01 00:45:55 -0400

    Update Data_Processing.py

--- Lib/Data_Processing.py
+++ Lib/Data_Processing.py
@@ -11,11 +11,15 @@ def Data_Division(dataset_dir):
 
     nC = 24
     (data_dir, mask_dir) = dataset_dir
+    if not os.path.exists(data_dir[0]):
+        os.makedirs(data_dir[0])
+    if not os.path.exists(data_dir[1]):
+        os.makedirs(data_dir[1])
     training_list = os.listdir(data_dir[0])
     valid_list = os.listdir(data_dir[1])
     testing_list = os.listdir(data_dir[2])
     mask_file = sio.loadmat(mask_dir + 'mask.mat')
-    mask = mask_file['mask']#[197:857,530:1190]
+    mask = mask_file['mask']
     mask_3d = np.tile(mask[:,:,np.newaxis],(1,1,nC))
     return data_dir, training_list, valid_list, testing_list, mask_3d
 
@@ -35,7 +39,7 @@ def Data_Generator_File(data_dir, data_list, mask, batch_size, is_training=True,
                 ind_set = sample_cnt
             if is_testing is True:
                 img = np.zeros((H,W,nC))
-                meas = sio.loadmat(data_dir[2] + data_list[ind_set])['meas']#[197:857,532:1238]
+                meas = sio.loadmat(data_dir[2] + data_list[ind_set])['meas']
                 meas = meas/meas.max()
             else:
                 if is_training is True:  
